8cb5a724d71ce83081c57b0466f62f1c61410e3b
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-11 15:23:14 +0500

    Code Format

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ var main = document.getElementById("main-content");
 // console.log(main.children)
 
 // iii
-var render = document.getElementsByClassName("render")
+var render = document.getElementsByClassName("render");
 
 // console.log(render)
 
-
 // iv
-function getValue(){
-    var firstName = document.getElementById("first-name")
-    firstName = firstName.value
-    console.log("First Name: " + firstName)
-    var lastName = document.getElementById("last-name")
-    lastName = lastName.value
-    console.log("Last Name: " + lastName)
-    var email = document.getElementById("email")
-    email = email.value
-    console.log("Email: " + email)
+function getValue() {
+  var firstName = document.getElementById("first-name");
+  firstName = firstName.value;
+  console.log("First Name: " + firstName);
+  var lastName = document.getElementById("last-name");
+  lastName = lastName.value;
+  console.log("Last Name: " + lastName);
+  var email = document.getElementById("email");
+  email = email.value;
+  console.log("Email: " + email);
 }
