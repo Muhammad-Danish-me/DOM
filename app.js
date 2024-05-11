35bb2982a2823c793858c7f9e082449cf3663085
Muhammad Danish <Muhammad.danish1015@gmail.com>
2024-05-11 15:22:21 +0500

    DOM

---
+++ app.js
@@ -0,0 +1,27 @@
+// Q.1
+// i
+var main = document.getElementById("main-content");
+// console.log(main)
+
+// ii
+var main = document.getElementById("main-content");
+// console.log(main.children)
+
+// iii
+var render = document.getElementsByClassName("render")
+
+// console.log(render)
+
+
+// iv
+function getValue(){
+    var firstName = document.getElementById("first-name")
+    firstName = firstName.value
+    console.log("First Name: " + firstName)
+    var lastName = document.getElementById("last-name")
+    lastName = lastName.value
+    console.log("Last Name: " + lastName)
+    var email = document.getElementById("email")
+    email = email.value
+    console.log("Email: " + email)
+}
